9311c797084fd870712ead59b248bce766b0cbd3
Atesh <73840304+Atesh1710@users.noreply.github.com>
2024-12-06 17:55:35 +0530

    Delete pages directory

--- pages/_app.js
+++
@@ -1,6 +0,0 @@
-// pages/_app.js
-import '../styles/globals.css'; // This file should include Tailwind setup
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
